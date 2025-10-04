5886050c3a87727dc0c8c3e6b41188e6338a6734
kinggodSson <kinggodsson@god.com>
2025-10-04 14:52:03 +0000

    레전드 질문부탁명령기타발언 분류 튜닝

--- src/app/services/llm.py
+++ src/app/services/llm.py
@@ -1,4 +1,13 @@
 from google import genai
+from google.genai import types
+import json
+import os
+
+questionTypeDeterminerPath = "utils/questionTypeChecker.json"
+
+def load_config(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 def createClient(key):
     try:
@@ -14,3 +23,28 @@ def getResponse(client, contents):
         contents=[contents]
     )
     return response
+
+def determineQuestionType(client, message):
+    config_data = load_config(questionTypeDeterminerPath)
+    contents = []
+    for ex in config_data["examples"]:
+        contents.append({"role": "user", "parts": [{"text": ex["input"]}]})
+        contents.append({"role": "model", "parts": [{"text": json.dumps(ex["output"])}]})
+    
+    contents.append({"role": "user", "parts": [{"text": message}]})
+    
+    api_config = types.GenerateContentConfig(
+        system_instruction=config_data["system_instruction"],
+        response_mime_type = config_data["output_mime_type"],
+    )
+
+    response = client.models.generate_content(
+        model="gemini-flash-latest",
+        contents=contents,
+        config=api_config
+    )
+    classification = json.loads(response.text)
+    return classification
+
+client = createClient("AIzaSyAImTpH0XqZNe1bwj-ShAlyhozNILxUauw")
+print(determineQuestionType(client, "날이 더워"))
