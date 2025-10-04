a20eacc377cf9c9a2d2b649030d7c906ecbb1176
kinggodSson <kinggodsson@god.com>
2025-10-04 13:37:46 +0000

    Gemini API Basic structure(createClient, getResponse) 걍 모듈화할라니까 알아서 다들 따르도록.

--- src/app/services/llm.py
+++ src/app/services/llm.py
@@ -1,2 +1,16 @@
-print("Hello World")
-print("yee")
+from google import genai
+
+def createClient(key):
+    try:
+        client = genai.Client(api_key=key)
+        return client
+    except:
+        print("Not able to get client")
+        return None
+
+def getResponse(client, contents):
+    response = client.models.generate_content(
+        model = "gemini-flash-latest",
+        contents=[contents]
+    )
+    return response
